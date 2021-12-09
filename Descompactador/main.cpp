d3051aef017ae0f75fa0a7e23999e42050372c50
Terada <teradaandre@gmail.com>
2021-12-09 17:17:34 -0300

    Acho q resolveu o problema do nome

--- Descompactador/main.cpp
+++ Descompactador/main.cpp
@@ -17,35 +17,6 @@ int main()
     cin >> FileName;
     strcat(FilePath, FileName);
     strcat(FilePath, ".mali");
-    /*
-        // Abre o arquivo e copia todos os bytes para dentro de um vetor
-        ifstream Input(FilePath, ios::binary);
-
-        if (Input)
-        {
-            // get length of file:
-            Input.seekg(0, Input.end);
-            int Length = Input.tellg();
-            Input.seekg(0, Input.beg);
-
-            BYTE *Buffer = new BYTE[Length];
-
-            cout << "Reading " << Length << " characters... ";
-            // read data as a block:
-            Input >> TamSaida;
-            Input >> TamArvore;
-            Input >> TamTexto;
-            printf("%d | %d | %d\n", TamSaida, TamArvore, TamTexto);
-            for(int i = 7; i < Length; i++)
-                Input >> Buffer[i];
-
-            if (Input)
-                cout << "all characters read successfully.";
-            else
-                cout << "error: only " << Input.gcount() << " could be read";
-        }
-
-        vector<BYTE> Buffer(istreambuf_iterator<char>(Input), {});*/
 
     FILE *Arquivo = fopen(FilePath, "rb");
     if (Arquivo == NULL)
@@ -54,7 +25,7 @@ int main()
     {
         fseek(Arquivo, 0L, SEEK_END);
         int Length = ftell(Arquivo);
-        rewind (Arquivo);
+        rewind(Arquivo);
 
         printf("Tamanho do arquivo: %d bytes\n", Length);
         BYTE TamSaida;
@@ -65,7 +36,7 @@ int main()
         fread(&TamTextoComp, sizeof(int), 1, Arquivo);
         printf("%d | %d | %d\n", TamSaida, TamArvore, TamTextoComp);
 
-        char CaminhoSaida[TamSaida] = {0};
+        char CaminhoSaida[TamSaida + 1] = {0};
         BYTE ArvBuilder[TamArvore] = {0};
         BYTE TextBuffer[TamTextoComp] = {0};
 
@@ -73,11 +44,10 @@ int main()
         fread(ArvBuilder, sizeof(BYTE), TamArvore, Arquivo);
         fread(TextBuffer, sizeof(BYTE), TamTextoComp, Arquivo);
 
-
-        printf("Caminho saida: %s\n", CaminhoSaida);
+        printf("Tamnho Saida: %d Caminho saida: %s\n", TamSaida, CaminhoSaida);
 
         Descompactador Descompactador(ArvBuilder);
-        Descompactador.GerarDiagramaDeArvore();
+        // Descompactador.GerarDiagramaDeArvore();
 
         ofstream Output;
         char OutFilePath[100] = "C:\\temp\\";
